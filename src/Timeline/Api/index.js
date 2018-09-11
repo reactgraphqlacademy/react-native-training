2b9917aa1b7f3c3f13f4721bd28a52ea0ca5af03
Horacio Herrera <hherrerag@me.com>
2018-09-11 14:28:23 +0200

    loading flatlist with lcoal content

---
+++ src/Timeline/Api/index.js
@@ -0,0 +1,5 @@
+import timeline from './data/timeline.json'
+
+export const fetchTimeline = () => {
+  return Promise.resolve(timeline)
+}
--- src/Timeline/Screens/TimelineScreen.js
+++ src/Timeline/Screens/TimelineScreen.js
@@ -1,11 +1,50 @@
-import react from 'react';
-import { View, Text} from 'react-native';
 import React from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import * as api from '../Api'
 
-const TimelineScreen = () => (
-  <View>
-    <Text>HELLO TIMELINE</Text>
-  </View>
-)
+// fetch timeline
+// display all the tweets in a flatList
+
+class TimelineScreen extends React.Component {
+  static navigationOptions = () => ({
+    title: "Timeline"
+  });
+
+  state = {
+    timeline: []
+  }
+
+  fetchTimeline = () => {
+    this.props.api.fetchTimeline().then(timeline => {
+      this.setState({timeline});
+    })
+  }
+
+  componentDidMount() {
+    this.fetchTimeline();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.timeline}
+          renderItem={({item}) => <Text>{item.id}</Text>}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+TimelineScreen.defaultProps = {
+  api
+}
 
 export default TimelineScreen
