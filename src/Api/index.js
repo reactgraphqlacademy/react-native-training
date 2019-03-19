77314fee8a94d58013bb1787fb5725cfe724c0c3
Horacio Herrera <me@hherrerag.com>
2019-03-19 15:27:56 +0100

    fix setup

--- src/Api/index.js
+++ src/Api/index.js
@@ -11,7 +11,7 @@ export const fetchTimeline = () => {
   //   .then(data => {
   //   return data.statuses;
   // })
-  return timeline
+  return Promise.resolve(timeline);
 }
 
 export const fetchTweetById = ({ id }) => {
@@ -21,7 +21,7 @@ export const fetchTweetById = ({ id }) => {
   //     console.log(data);
   //   return data;
   // })
-  return tweet
+  return Promise.resolve(tweet);
 }
 
 export const fetchUser = ({ userId, name }) => {
@@ -30,7 +30,7 @@ export const fetchUser = ({ userId, name }) => {
   //   .then(data => {
   //     return data;
   // });
-  return user
+  return Promise.resolve(user);
 }
 
 export const fetchUsersTimeline = ({ userId, name }) => {
@@ -39,5 +39,5 @@ export const fetchUsersTimeline = ({ userId, name }) => {
   //   .then(data => {
   //     return data;
   // });
-  return userTimeline
+  return Promise.resolve(userTimeline);
 }
