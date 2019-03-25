95639af573ad7cc92294b92682a3b350d8eee8e2
Alex Lobera <alex.lobera@gmail.com>
2019-03-25 13:40:00 +0000

    Update RootNavigator.js

--- src/Screens/RootNavigator.js
+++ src/Screens/RootNavigator.js
@@ -7,10 +7,9 @@ export const PRIVATE_SCREEN = "PRIVATE_SCREEN";
 export const PUBLIC_SCREEN = "PUBLIC_SCREEN";
 
 /*
-- create a Switch Navigator called `MainNavigator` with a PRIVATE and a PUBLIC views
-- this navigator should not have a header (hint: `headerMode`)
-- this is the initial navigator for the App, so you need to wrap
-  the entry point of your app with `createAppContainer`
+- Create a Switch Navigator called `MainNavigator` with a PRIVATE and a PUBLIC views
+- This is the initial navigator for the App, so you need to wrap
+  the entry point of your app with `createAppContainer`. You can check the documentation here https://reactnavigation.org/docs/en/app-containers.html
 */
 
 const MainNavigator = "create a switch navigator here";
