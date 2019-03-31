2a9ab46e837ca177d0a972779cc249c9c98e2c40
alexlbr <alex.lobera@gmail.com>
2019-03-31 14:45:45 +0100

    adds RootNavigator for exercise 1

--- src/Screens/RootNavigator.js
+++ src/Screens/RootNavigator.js
@@ -1,17 +1,19 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation'
-import { PublicNavigator } from './PublicNavigator'
-import { PrivateNavigator } from './PrivateNavigator'
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import { PublicNavigator } from "./PublicNavigator";
+import { PrivateNavigator } from "./PrivateNavigator";
+import { LoginScreen } from "./LoginScreen";
 
-export const PRIVATE_SCREEN = 'PRIVATE_SCREEN'
-export const PUBLIC_SCREEN = 'PUBLIC_SCREEN'
+export const PRIVATE_SCREEN = "PRIVATE_SCREEN";
+export const PUBLIC_SCREEN = "PUBLIC_SCREEN";
 
 /*
+EXERCISE PART 1 
+- This is the initial navigator for the App, so you need to wrap
+  the entry point of your app with `createAppContainer` using the PublicNavigator
+
+EXERCISE PART 2 (HEADS UP! DON'T DO THIS UNTIL PART 2)
 - create a Switch Navigator called `MainNavigator` with a PRIVATE and a PUBLIC views
 - this navigator should not have a header (hint: `headerMode`)
-- this is the initial navigator for the App, so you need to wrap
-  the entry point of your app with `createAppContainer`
 */
 
-const MainNavigator = "create a switch navigator here"
-
-export const RootNavigator = "you should call a function here passing the `MainNavigator`"
+export const RootNavigator = LoginScreen;
