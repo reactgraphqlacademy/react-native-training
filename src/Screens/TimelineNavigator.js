ace8e2fbbb43e4d4e84d54612af2d880c5d6795e
alexlbr <alex.lobera@gmail.com>
2019-03-31 15:40:43 +0100

    adds solution exercise 3

--- src/Screens/TimelineNavigator.js
+++ src/Screens/TimelineNavigator.js
@@ -1,24 +1,27 @@
 import { createStackNavigator } from "react-navigation";
 import { Colors } from "../config/utils";
 import { TimelineScreen } from "./TimelineScreen";
+import { TweetDetailScreen } from "./TweetDetailScreen";
+import { ProfileScreen } from "./ProfileScreen";
 
 export const MAIN_TIMELINE_SCREEN = "MAIN_TIMELINE_SCREEN";
 export const TIMELINE_FEED_SCREEN = "TIMELINE_FEED_SCREEN";
 export const TWEET_DETAIL_SCREEN = "TWEET_DETAIL_SCREEN";
 export const PROFILE_SCREEN = "PROFILE_SCREEN";
-export const OTHER_USER_PROFILE = "OTHER_USER_PROFILE";
+export const USER_PROFILE = "USER_PROFILE";
 
-/*
-- create a stach Navigator for the TimeLine tab.
-
-```
-  [TIMELINE_FEED_SCREEN]: TimelineScreen,
-  [TWEET_DETAIL_SCREEN]: TweetDetailScreen,
-  [OTHER_USER_PROFILE]: ProfileScreen,
-
-```
-- the `header backgroundColor for this navigator should be `Colors.brand.primary`
-  and the header content is white.
-*/
-
-export const TimelineNavigator = TimelineScreen;
+export const TimelineNavigator = createStackNavigator(
+  {
+    [TIMELINE_FEED_SCREEN]: TimelineScreen,
+    [TWEET_DETAIL_SCREEN]: TweetDetailScreen,
+    [USER_PROFILE]: ProfileScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Colors.brand.primary
+      },
+      headerTintColor: Colors.light
+    }
+  }
+);
