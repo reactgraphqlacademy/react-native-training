542d51cbca35dddf69df01adbd643ec4e0d85911
Horacio Herrera <me@hherrerag.com>
2019-04-10 18:46:31 +0200

    update api + add babel-transform

--- src/Api/index.js
+++ src/Api/index.js
@@ -3,41 +3,10 @@ import tweet from './tweet'
 import user from './user'
 import userTimeline from './userTimeline'
 
-// const API_URL = ""
+export const fetchTimeline = () => Promise.resolve(timeline);
 
-export const fetchTimeline = () => {
-  // return fetch(`${API_URL}/tweets`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //   return data.statuses;
-  // })
-  return Promise.resolve(timeline);
-}
+export const fetchTweetById = ({ id }) => Promise.resolve(tweet);
 
-export const fetchTweetById = ({ id }) => {
-  // return fetch(`${API_URL}/tweets/${id}`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     console.log(data);
-  //   return data;
-  // })
-  return Promise.resolve(tweet);
-}
+export const fetchUser = ({ userId, name }) => Promise.resolve(user);
 
-export const fetchUser = ({ userId, name }) => {
-  // return fetch(`${API_URL}/users/${userId}/${name}`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     return data;
-  // });
-  return Promise.resolve(user);
-}
-
-export const fetchUsersTimeline = ({ userId, name }) => {
-  // return fetch(`${API_URL}/users/${userId}/${name}/timeline`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     return data;
-  // });
-  return Promise.resolve(userTimeline);
-}
+export const fetchUsersTimeline = ({ userId, name }) => Promise.resolve(userTimeline);
