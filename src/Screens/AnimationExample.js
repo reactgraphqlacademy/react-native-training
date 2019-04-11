8f8fac53634ba86e9cea0e429dfd77c285ce337a
alexlbr <alex.lobera@gmail.com>
2019-04-11 14:37:32 +0200

    updates AnimationExample.js

--- src/Screens/AnimationExample.js
+++ src/Screens/AnimationExample.js
@@ -3,104 +3,59 @@ import { View, Animated } from "react-native";
 
 export class AnimationExample extends React.Component {
   x = new Animated.Value(0);
-
-  render() {
-    return (
-      <View
-        style={{
-          backgroundColor: "#6ECFF0",
-          height: 100,
-          width: 100
-          //,transform: [{ translateX: 100 }]
-        }}
-      />
-    );
-  }
-}
-
-export class AnimationExample extends React.Component {
-  x = new Animated.Value(0);
   opacity = new Animated.Value(1);
+  color = new Animated.Value(0);
 
   componentDidMount() {
     Animated.timing(this.x, {
-      duration: 3000,
+      duration: 2000,
       toValue: 300
     }).start();
 
     Animated.timing(this.opacity, {
-      duration: 4000,
+      duration: 2000,
       toValue: 0
+    }).start();
+
+    Animated.timing(this.color, {
+      duration: 2000,
+      toValue: 255
     }).start();
   }
 
   render() {
+    // if you read the docs and you found multiply
+    let y = Animated.multiply(this.x, 2);
+    let color = this.color.interpolate({
+      inputRange: [0, 255],
+      outputRange: [`rgba(255,0,0,1)`, `rgba(0,255,0,1)`]
+    });
+    /*
+    hacky (and worse) solution using interpolation if you don't read the docs xD
+    let y = this.x.interpolate({
+      inputRange: [0,300],
+      outputRange: [0,600]
+    })
+    */
+
+    // you can create a new animation for the opacity or interpolate the values
+    let { opacity } = this;
+    // or we can interpolate it, just for fun :D
+    // let opacity = this.x.interpolate({
+    //   inputRange: [0, 300],
+    //   outputRange: [1, 0]
+    // });
+
     return (
       <Animated.View
         style={{
-          backgroundColor: "#6ECFF0",
+          backgroundColor: color,
           height: 100,
           width: 100,
-          opacity: this.opacity,
-          transform: [{ translateX: this.x }, { translateY: this.x }]
+          opacity,
+          transform: [{ translateX: this.x }, { translateY: y }]
         }}
       />
     );
   }
 }
-
-// import * as React from "react";
-// import { Text, View, StyleSheet, Animated } from "react-native";
-
-// export class AnimationExample extends React.Component {
-//   state = {
-//     opacity: new Animated.Value(0),
-//     color: new Animated.Value(0)
-//   };
-
-//   componentDidMount() {
-//     Animated.timing(this.state.opacity, {
-//       toValue: 1,
-//       duration: 2000
-//     }).start();
-//     Animated.timing(this.state.color, {
-//       toValue: 255,
-//       duration: 2000
-//     }).start();
-//   }
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Animated.View
-//           style={{
-//             opacity: this.state.opacity,
-//             backgroundColor: this.state.color.interpolate({
-//               inputRange: [0, 255],
-//               outputRange: ["rgba(0,0,255,1)", "rgba(255,0,0,1)"]
-//             })
-//           }}
-//         >
-//           <Text style={styles.paragraph}>
-//             Change code!! in the editor and watch it change on your phone! Save
-//             to get a shareable url. 123
-//           </Text>
-//         </Animated.View>
-//       </View>
-//     );
-//   }
-// }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
-//     backgroundColor: "#ecf0f1"
-//   },
-//   paragraph: {
-//     margin: 24,
-//     fontSize: 18,
-//     fontWeight: "bold",
-//     textAlign: "center",
-//     color: "#34495e"
-//   }
-// });
