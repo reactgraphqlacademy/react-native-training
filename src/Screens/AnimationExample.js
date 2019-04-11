ec5afbf25f43f2e489a2d38b4201b044f896a088
alexlbr <alex.lobera@gmail.com>
2019-04-11 14:46:58 +0200

    updates AnimationExample.js

--- src/Screens/AnimationExample.js
+++ src/Screens/AnimationExample.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Animated } from "react-native";
 
 export class AnimationExample extends React.Component {
+  state = {};
   render() {
     return (
       <View
