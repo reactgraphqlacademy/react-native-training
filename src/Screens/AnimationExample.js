5bd73249287ea58433a1d495dc93f8e2cc413d32
alexlbr <alex.lobera@gmail.com>
2019-04-11 08:44:59 +0200

    updates Animation Example

--- src/Screens/AnimationExample.js
+++ src/Screens/AnimationExample.js
@@ -2,16 +2,13 @@ import React from "react";
 import { View, Animated } from "react-native";
 
 export class AnimationExample extends React.Component {
-  x = new Animated.Value(0);
-
   render() {
     return (
       <View
         style={{
           backgroundColor: "#6ECFF0",
           height: 100,
-          width: 100,
-          transform: [{ translateX: 100 }]
+          width: 100
         }}
       />
     );
